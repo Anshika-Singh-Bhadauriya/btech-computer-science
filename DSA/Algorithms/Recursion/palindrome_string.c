74a9a0fc3a9ecd63974a57613ea3ba4bdc08e68a
Anshika-Singh-Bhadauriya <2k25cse2510474@gmail.com>
2026-09-12 19:51:43 +0530

    Refactor palindrome check to Java from C

--- DSA/Algorithms/Recursion/palindrome_string.c
+++ DSA/Algorithms/Recursion/palindrome_string.c
@@ -2,26 +2,27 @@
 // Approach: Compare characters at the start and end indices;
 // if they match, recurse inward; if they don't, it's not a palindrome.
 
-#include<stdio.h>
-#include<string.h>
+import java.util.Scanner;
 
-void palindrome(char str[], int start, int end){
-    if(start >= end){
-        printf("\npalindrome");
-        return;
-    }
-    if(str[start] != str[end]){
-        printf("\nnot palindrome");
-        return;
-    }
-    palindrome(str, start+1, end-1);
-}
+public class PalindromeCheck {
 
-int main(){
-    char str[100];
-    printf("enter string:");
-    scanf("%s", str);
-    int end = strlen(str) - 1;
-    palindrome(str, 0, end);
-    return 0;
+    static void palindrome(String str, int start, int end){
+        if(start >= end){
+            System.out.println("\npalindrome");
+            return;
+        }
+        if(str.charAt(start) != str.charAt(end)){
+            System.out.println("\nnot palindrome");
+            return;
+        }
+        palindrome(str, start+1, end-1);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter string:");
+        String str = sc.next();
+        int end = str.length() - 1;
+        palindrome(str, 0, end);
+    }
 }
